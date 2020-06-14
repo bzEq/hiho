7a66fa3c8c71586b6b494118e3950874653bb7d1
Kai Luo <gluokai@gmail.com>
2020-06-15 01:24:59 +0800

    Remove Glass

--- tools/demo_scene.cc
+++ tools/demo_scene.cc
@@ -12,7 +12,6 @@ int main(int argc, char *argv[]) {
   World scene;
   Mirror mirror;
   Solid solid;
-  Glass glass(1.05);
   scene.AddObject<Sphere>(Vec3f{0, 0, 50}, 50, &solid, Vec3f{0, 0, 0});
   scene.AddObject<Sphere>(Vec3f{-200, -200, 120}, 120, &mirror, Vec3f{0, 0, 0});
   scene.AddObject<Sphere>(Vec3f{-100, 200, 100}, 100, &solid, Vec3f{0, 0, 0});
@@ -20,7 +19,6 @@ int main(int argc, char *argv[]) {
   scene.AddObject<Sphere>(Vec3f{100, 40, 20}, 20, &solid, Vec3f{0, 0, 0});
   scene.AddObject<Sphere>(Vec3f{150, 100, 40}, 40, &mirror, Vec3f{0, 0, 0});
   scene.AddObject<Sphere>(Vec3f{100, -100, 80}, 80, &mirror, Vec3f{0, 0, 0});
-  // scene.AddObject<Sphere>(Vec3f{150, 0, 80}, 80, &glass, Vec3f{0, 0, 0});
   // Light.
   scene.AddObject<Sphere>(Vec3f{1e7, 0, 0}, 1e7 - 1e5, &solid,
                           Vec3f{10, 10, 10});
