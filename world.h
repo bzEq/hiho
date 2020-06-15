88aadda1e7e05c027de00cc365ae29af7fe11934
Kai Luo <gluokai@gmail.com>
2020-06-15 09:19:46 +0800

    Remove EmitEnergy

--- world.h
+++ world.h
@@ -29,11 +29,6 @@ struct World : GeometryConcept {
       return i;
     return std::nullopt;
   }
-
-  virtual Vec3f EmitEnergy(const Vec3f &point,
-                           const Vec3f &direction) const override {
-    return Vec3f{0, 0, 0};
-  }
 };
 
 } // namespace hiho
