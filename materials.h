17a459bd59e9cf2135f9a5c705a76de90dabb17b
Kai Luo <gluokai@gmail.com>
2020-06-15 10:09:25 +0800

    Inverse normal inside object

--- materials.h
+++ materials.h
@@ -9,12 +9,13 @@ struct Mirror : MaterialConcept {
                                 const Vec3f &normal) const override {
     std::vector<Scattery> result;
     Scattery s;
-    if (in.dot(normal) < 0) {
-      s.scattered.origin = point;
-      s.scattered.direction = Reflect(in, normal);
-      s.pdf = Vec3f{1, 1, 1};
-      result.push_back(std::move(s));
-    }
+    Vec3f n(normal);
+    if (in.dot(n) > 0)
+      n = -n;
+    s.scattered.origin = point;
+    s.scattered.direction = Reflect(in, n);
+    s.pdf = Vec3f{1, 1, 1};
+    result.push_back(std::move(s));
     return result;
   }
 };
@@ -27,15 +28,16 @@ struct Solid : MaterialConcept {
   std::vector<Scattery> Scatter(const Vec3f &point, const Vec3f &in,
                                 const Vec3f &normal) const override {
     std::vector<Scattery> result;
-    if (in.dot(normal) > 0)
-      return result;
+    Vec3f n(normal);
+    if (in.dot(n) > 0)
+      n = -n;
     Scattery s;
     Vec3f out = SampleFromSphere();
-    if (out.dot(normal) < 0)
+    if (out.dot(n) < 0)
       out = -out;
     s.scattered.origin = point;
     s.scattered.direction = out;
-    s.pdf = color * normal.dot(-in) / PI;
+    s.pdf = color * n.dot(-in) / PI;
     result.push_back(std::move(s));
     return result;
   }
